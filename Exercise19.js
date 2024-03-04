353b1f1ad296688b2e1e91d8ee85f28cb0493feb
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-04 23:29:08 +0500

    "Exercise19" completed.

---
+++ Exercise19.js
@@ -0,0 +1,10 @@
+"use strict";
+/***
+ *
+ * Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+ *  print a message indicating the number of people you are inviting to dinner.
+ *
+ *
+*/
+let guestList = ["A", "b", 'c', `d`];
+console.log("Guest are invited : ", guestList.length);
---
+++ Exercise19.ts
@@ -0,0 +1,10 @@
+/***
+ * 
+ * Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+ *  print a message indicating the number of people you are inviting to dinner.
+ * 
+ * 
+*/
+
+let guestList:string[]=["A","b",'c',`d`]
+console.log("Guest are invited : ", guestList.length)
