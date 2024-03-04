58726fc3ee8ae865a134be6f38f8009c5fc0971a
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-04 23:23:15 +0500

    "Exercise18" is completed.

---
+++ Exercise18.ts
@@ -0,0 +1,46 @@
+/***
+ * Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a array. Make sure the array is not in alphabetical order.
+
+• Print your array in its original order.
+
+• Print your array in alphabetical order without modifying the actual list.
+
+• Show that your array is still in its original order by printing it.
+
+• Print your array in reverse alphabetical order without changing the order of the original list.
+
+• Show that your array is still in its original order by printing it again.
+
+• Reverse the order of your list. Print the array to show that its order has changed.
+
+• Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+ * 
+ * 
+ */
+
+let wishList:string[]=["Tokyo","Beggin","Berlin","Paris","Quetta"];
+
+console.log("Origional Order", wishList);
+
+
+let newSortedItemList:string[]=wishList;
+console.log("Without Sorting List: ", newSortedItemList);
+newSortedItemList.slice().sort();
+console.log("Sorted List: ",newSortedItemList);
+
+console.log("Actual Array is still",wishList)
+
+let reveseSortedListItem:string[]=wishList;
+reveseSortedListItem.sort((a,b)=>b.localeCompare(a));
+console.log("Items are in reversed order",reveseSortedListItem)
+
+console.log("String order is same after reversing",wishList);
+
+console.log("Sorted your own array",wishList.sort())
+
+console.log("Reverse your items sorted",wishList.reverse())
