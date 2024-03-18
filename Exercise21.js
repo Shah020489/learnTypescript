353e53ea75bf8a62d5d47bdeb130271f6cc4b1b1
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-18 20:58:00 +0500

    Changes are added in it.

---
+++ Exercise21.js
@@ -0,0 +1,18 @@
+"use strict";
+/***
+ * They think of something you could store in a TypeScript Object.
+ *  Write a program that creates Objects containing these items.
+ */
+let objLanuguage = {
+    urdu: "Urud",
+    punjabi: "Punjabi",
+    sindhi: "Sindhi",
+    bangoli: "Bangoli"
+};
+console.log(objLanuguage);
+/** After getting values of object */
+console.log("Getting object value By the key");
+console.log(objLanuguage.urdu);
+console.log(objLanuguage.punjabi);
+console.log(objLanuguage.sindhi);
+console.log(objLanuguage.bangoli);
---
+++ Exercise21.ts
@@ -0,0 +1,20 @@
+/***
+ * They think of something you could store in a TypeScript Object.
+ *  Write a program that creates Objects containing these items.
+ */
+
+
+let objLanuguage:Object={
+    urdu:"Urud",
+    punjabi:"Punjabi",
+    sindhi:"Sindhi",
+    bangoli:"Bangoli"
+}
+
+console.log(objLanuguage);
+/** After getting values of object */
+console.log("Getting object value By the key");
+console.log(objLanuguage.urdu);
+console.log(objLanuguage.punjabi);
+console.log(objLanuguage.sindhi);
+console.log(objLanuguage.bangoli);
